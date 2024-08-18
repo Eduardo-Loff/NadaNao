d6d4b3b49fe2aa9a9fdf37d6132e678e0306de5c
Eduardo Felipe <156210174+Eduardo-Loff@users.noreply.github.com>
2024-08-18 17:50:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrir(){
+    let area = document.getElementById('surpresa');
+    var audio = new Audio('rojao.mp3');
+    audio.play();
+    area.innerHTML += ``
+    area.innerHTML += `<p>QQ TU TAVA ESPERANDO CARAIKKKKKKKKKKK</p>`
+    area.innerHTML += `<p>pfv, me da dinheiro n guento mais ser pobre </p>`
+    
+}
